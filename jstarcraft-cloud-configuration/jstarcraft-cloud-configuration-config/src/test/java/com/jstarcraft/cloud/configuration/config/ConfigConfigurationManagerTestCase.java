06ff2bcc23903fd933d60c3a6a57bfe383bd5d1e
Birdy <Birdy@LAPTOP-QRG8T75T>
2019-05-18 12:41:56 +0800

    测试Spring Cloud Config配置刷新.

---
+++ jstarcraft-cloud-configuration/jstarcraft-cloud-configuration-config/src/test/java/com/jstarcraft/cloud/configuration/config/ConfigConfigurationManagerTestCase.java
@@ -0,0 +1,52 @@
+package com.jstarcraft.cloud.configuration.config;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConfigConfigurationManagerTestCase {
+
+    @Autowired
+    private ConfigConfigurationManager configurationManager;
+
+    @Autowired
+    private ConfigProperties properties;
+
+    /**
+     * 测试刷新
+     */
+    @Test
+    public void testRefresh() {
+
+        Assert.assertEquals("old-config", properties.getConfig());
+
+        WebClient webClient = WebClient.create();
+        {
+            // 设置新配置
+            webClient.get()
+                    // 设置URL
+                    .uri("http://localhost:8888/config/update2New").retrieve().bodyToMono(String.class).block();
+        }
+
+        {
+            // 触发刷新配置
+            configurationManager.refresh();
+        }
+
+        Assert.assertEquals("new-config", properties.getConfig());
+
+        {
+            // 设置旧配置
+            webClient.get()
+                    // 设置URL
+                    .uri("http://localhost:8888/config/update2Old").retrieve().bodyToMono(String.class).block();
+        }
+    }
+
+}
