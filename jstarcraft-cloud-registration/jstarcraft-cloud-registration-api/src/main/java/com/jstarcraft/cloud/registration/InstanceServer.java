58e2e2433d09e69ac2b36747da97aee74e8a5914
Birdy <Birdy@LAPTOP-QRG8T75T>
2019-05-18 09:45:09 +0800

    实现基于ServiceInstance的ServerList.便于自定义路由策略与自定义Ribbon客户端.

---
+++ jstarcraft-cloud-registration/jstarcraft-cloud-registration-api/src/main/java/com/jstarcraft/cloud/registration/InstanceServer.java
@@ -0,0 +1,27 @@
+package com.jstarcraft.cloud.registration;
+
+import java.util.Map;
+
+import org.springframework.cloud.client.ServiceInstance;
+
+import com.netflix.loadbalancer.Server;
+
+/**
+ * 实例服务
+ * @author Birdy
+ *
+ */
+public class InstanceServer extends Server {
+
+    protected ServiceInstance instance;
+
+    public InstanceServer(ServiceInstance instance) {
+        super(instance.getScheme(), instance.getHost(), instance.getPort());
+        this.instance = instance;
+    }
+
+    public Map<String, String> getMetadata() {
+        return instance.getMetadata();
+    }
+
+}
