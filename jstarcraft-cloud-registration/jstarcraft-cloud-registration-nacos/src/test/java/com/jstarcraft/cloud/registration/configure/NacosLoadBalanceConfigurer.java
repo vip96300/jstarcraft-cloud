1d1e70d1a7017e25f46d8ea247a8f0c482f0d377
Birdy <Birdy@LAPTOP-QRG8T75T>
2020-08-05 17:33:14 +0800

    测试NacosServerManager.

---
+++ jstarcraft-cloud-registration/jstarcraft-cloud-registration-nacos/src/test/java/com/jstarcraft/cloud/registration/configure/NacosLoadBalanceConfigurer.java
@@ -0,0 +1,32 @@
+package com.jstarcraft.cloud.registration.configure;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.cloud.netflix.ribbon.PropertiesFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.alibaba.cloud.nacos.NacosDiscoveryProperties;
+import com.alibaba.cloud.nacos.ribbon.NacosRibbonClientConfiguration;
+import com.alibaba.cloud.nacos.ribbon.NacosServerList;
+import com.jstarcraft.cloud.registration.nacos.NacosServerManager;
+import com.netflix.client.config.IClientConfig;
+import com.netflix.loadbalancer.ServerList;
+
+@Configuration
+@AutoConfigureAfter(NacosRibbonClientConfiguration.class)
+// 此类是为了演示如何覆盖Spring Cloud的自动装配
+public class NacosLoadBalanceConfigurer {
+
+    @Autowired
+    private PropertiesFactory propertiesFactory;
+
+    @Bean
+    public ServerList<?> ribbonServerList(IClientConfig config, NacosDiscoveryProperties properties) {
+        NacosServerList serverList = new NacosServerList(properties);
+        serverList.initWithNiwsConfig(config);
+        NacosServerManager manager = new NacosServerManager(config, serverList);
+        return manager;
+    }
+
+}
