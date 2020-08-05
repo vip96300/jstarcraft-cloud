1069a32432c8a2a9871dc095d4d8d0fafd95cd6b
Birdy <Birdy@LAPTOP-QRG8T75T>
2020-08-05 18:32:16 +0800

    重构单元测试.

--- jstarcraft-cloud-registration/jstarcraft-cloud-registration-eureka/src/test/java/com/jstarcraft/cloud/registration/eureka/EurekaApplication.java
+++ jstarcraft-cloud-registration/jstarcraft-cloud-registration-eureka/src/test/java/com/jstarcraft/cloud/registration/eureka/EurekaApplication.java
@@ -1,19 +1,19 @@
-package com.jstarcraft.cloud.registration.nacos;
+package com.jstarcraft.cloud.registration.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
-import com.jstarcraft.cloud.registration.configure.NacosLoadBalanceConfigurer;
+import com.jstarcraft.cloud.registration.configurer.EurekaLoadBalanceConfigurer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@RibbonClient(name = "demo", configuration = NacosLoadBalanceConfigurer.class)
-public class MockNacosApplication {
+@RibbonClient(name = "demo", configuration = EurekaLoadBalanceConfigurer.class)
+public class EurekaApplication {
 
     public static void main(String[] arguments) {
-        SpringApplication.run(MockNacosApplication.class, arguments);
+        SpringApplication.run(EurekaApplication.class, arguments);
     }
 
 }
--- jstarcraft-cloud-registration/jstarcraft-cloud-registration-nacos/src/test/java/com/jstarcraft/cloud/registration/nacos/NacosApplication.java
+++ jstarcraft-cloud-registration/jstarcraft-cloud-registration-nacos/src/test/java/com/jstarcraft/cloud/registration/nacos/NacosApplication.java
@@ -1,19 +1,19 @@
-package com.jstarcraft.cloud.registration.consul;
+package com.jstarcraft.cloud.registration.nacos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
-import com.jstarcraft.cloud.registration.configurer.ConsulLoadBalanceConfigurer;
+import com.jstarcraft.cloud.registration.configure.NacosLoadBalanceConfigurer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@RibbonClient(name = "demo", configuration = ConsulLoadBalanceConfigurer.class)
-public class MockConsulApplication {
+@RibbonClient(name = "demo", configuration = NacosLoadBalanceConfigurer.class)
+public class NacosApplication {
 
     public static void main(String[] arguments) {
-        SpringApplication.run(MockConsulApplication.class, arguments);
+        SpringApplication.run(NacosApplication.class, arguments);
     }
 
 }
