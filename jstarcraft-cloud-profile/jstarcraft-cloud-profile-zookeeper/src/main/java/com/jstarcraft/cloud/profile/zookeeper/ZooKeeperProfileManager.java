012e17ceb95d50f7f9baad4e3a4aeffe2596160c
Birdy <Birdy@LAPTOP-QRG8T75T>
2020-08-17 21:10:28 +0800

    实现与测试ZooKeeperProfileManager.

--- jstarcraft-cloud-profile/jstarcraft-cloud-profile-zookeeper/src/main/java/com/jstarcraft/cloud/profile/zookeeper/ZooKeeperProfileManager.java
+++ jstarcraft-cloud-profile/jstarcraft-cloud-profile-zookeeper/src/main/java/com/jstarcraft/cloud/profile/zookeeper/ZooKeeperProfileManager.java
@@ -1,11 +1,17 @@
 package com.jstarcraft.cloud.profile.zookeeper;
 
+import org.apache.curator.framework.CuratorFramework;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.jstarcraft.cloud.profile.ProfileManager;
 import com.jstarcraft.cloud.profile.ProfileMonitor;
 import com.jstarcraft.core.common.configuration.Configurator;
+import com.jstarcraft.core.common.configuration.JsonConfigurator;
+import com.jstarcraft.core.common.configuration.PropertyConfigurator;
+import com.jstarcraft.core.common.configuration.XmlConfigurator;
+import com.jstarcraft.core.common.configuration.YamlConfigurator;
+import com.jstarcraft.core.utility.StringUtility;
 
 /**
  * ZooKeeper配置管理器
@@ -14,13 +20,40 @@ import com.jstarcraft.core.common.configuration.Configurator;
  *
  */
 public class ZooKeeperProfileManager implements ProfileManager {
-    
+
     private static final Logger logger = LoggerFactory.getLogger(ZooKeeperProfileManager.class);
+
+    private CuratorFramework zookeeper;
+
+    private String format;
+
+    private String path;
+
+    public ZooKeeperProfileManager(CuratorFramework zookeeper, String format, String path) {
+        this.zookeeper = zookeeper;
+        this.format = format;
+        this.path = path;
+    }
 
     @Override
     public Configurator getConfiguration(String name) {
-        // TODO Auto-generated method stub
-        return null;
+        try {
+            byte[] data = zookeeper.getData().forPath(path + "/" + name);
+            String value = new String(data, StringUtility.CHARSET);
+            switch (format) {
+            case "json":
+                return new JsonConfigurator(value);
+            case "properties":
+                return new PropertyConfigurator(value);
+            case "xml":
+                return new XmlConfigurator(value);
+            case "yaml":
+                return new YamlConfigurator(value);
+            }
+            throw new IllegalArgumentException();
+        } catch (Exception exception) {
+            throw new RuntimeException(exception);
+        }
     }
 
     @Override
---
+++ jstarcraft-cloud-profile/jstarcraft-cloud-profile-zookeeper/src/test/java/com/jstarcraft/cloud/profile/zookeeper/ZooKeeperProfileManagerTestCase.java
@@ -0,0 +1,42 @@
+package com.jstarcraft.cloud.profile.zookeeper;
+
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.RetryOneTime;
+import org.apache.zookeeper.CreateMode;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.jstarcraft.core.common.configuration.Configurator;
+import com.jstarcraft.core.utility.StringUtility;
+
+public class ZooKeeperProfileManagerTestCase {
+
+    private static CuratorFramework zookeeper;
+
+    @BeforeClass
+    public static void start() throws Exception {
+        zookeeper = CuratorFrameworkFactory.builder().connectString("127.0.0.1:2181").retryPolicy(new RetryOneTime(2000)).build();
+        zookeeper.start();
+    }
+
+    @AfterClass
+    public static void stop() throws Exception {
+        zookeeper.close();
+    }
+
+    @Test
+    public void test() throws Exception {
+        String path = "/group";
+        String name = "jstarcraft";
+        byte[] data = "race=random".getBytes(StringUtility.CHARSET);
+        zookeeper.create().creatingParentsIfNeeded().withMode(CreateMode.EPHEMERAL).forPath(path + "/" + name, data);
+        ZooKeeperProfileManager manager = new ZooKeeperProfileManager(zookeeper, "properties", path);
+        Configurator configurator = manager.getConfiguration("jstarcraft");
+        Assert.assertEquals("random", configurator.getString("race"));
+        zookeeper.delete().forPath(path + "/" + name);
+    }
+
+}
