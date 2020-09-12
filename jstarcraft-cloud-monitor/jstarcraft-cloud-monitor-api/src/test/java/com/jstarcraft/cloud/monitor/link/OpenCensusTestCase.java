d1e7041bec84eb92256bfc35c389fed0b8b45d87
Birdy <Birdy@LAPTOP-QRG8T75T>
2020-09-12 18:15:50 +0800

    重构单元测试.

--- jstarcraft-cloud-monitor/jstarcraft-cloud-monitor-api/src/test/java/com/jstarcraft/cloud/monitor/link/OpenCensusTestCase.java
+++ jstarcraft-cloud-monitor/jstarcraft-cloud-monitor-api/src/test/java/com/jstarcraft/cloud/monitor/link/OpenCensusTestCase.java
@@ -1,5 +1,7 @@
 package com.jstarcraft.cloud.monitor.link;
 
+import java.util.Collection;
+
 import org.junit.Test;
 
 import com.jstarcraft.core.utility.RandomUtility;
@@ -13,17 +15,39 @@ import io.opencensus.trace.Tracer;
 import io.opencensus.trace.Tracestate;
 import io.opencensus.trace.Tracestate.Entry;
 import io.opencensus.trace.Tracing;
+import io.opencensus.trace.config.TraceConfig;
+import io.opencensus.trace.config.TraceParams;
+import io.opencensus.trace.export.SpanData;
+import io.opencensus.trace.export.SpanExporter.Handler;
+import io.opencensus.trace.samplers.Samplers;
 
 public class OpenCensusTestCase {
 
     @Test
-    public void test() {
-        TraceId traceId = TraceId.generateRandomId(RandomUtility.getRandom());
-//        traceId = TraceId.INVALID;
-        SpanId spanId = SpanId.generateRandomId(RandomUtility.getRandom());
-//        spanId = SpanId.INVALID;
-        TraceOptions traceOption = TraceOptions.builder().build();
+    public void test() throws Exception {
+        // 获取追踪配置
+        TraceConfig traceConfig = Tracing.getTraceConfig();
+        // 获取追踪参数
+        TraceParams traceParams = traceConfig.getActiveTraceParams();
+        // 设置采样率为100%
+        traceParams = traceParams.toBuilder().setSampler(Samplers.alwaysSample()).build();
+        // 设置最总参数
+        traceConfig.updateActiveTraceParams(traceParams);
+        // 注册追踪单元处理器
+        Tracing.getExportComponent().getSpanExporter().registerHandler("mock", new Handler() {
+
+            @Override
+            public void export(Collection<SpanData> datas) {
+                System.out.println("export");
+                System.out.println(datas);
+            }
+
+        });
+
         Tracer tracer = Tracing.getTracer();
+        TraceId traceId = TraceId.generateRandomId(RandomUtility.getRandom());
+        SpanId spanId = SpanId.generateRandomId(RandomUtility.getRandom());
+        TraceOptions traceOption = TraceOptions.builder().build();
         Tracestate tracestate = Tracestate.builder().set("key", "value").build();
         SpanContext spanContext = SpanContext.create(traceId, spanId, traceOption, tracestate);
         System.out.println(traceId);
@@ -32,9 +56,13 @@ public class OpenCensusTestCase {
         Span span = tracer.spanBuilderWithRemoteParent("root", spanContext).startSpan();
         System.out.println(span.getContext().getTraceId());
         System.out.println(span.getContext().getSpanId());
-        for(Entry term : span.getContext().getTracestate().getEntries()) {
+        for (Entry term : span.getContext().getTracestate().getEntries()) {
             System.out.println(term.getKey() + ":" + term.getValue());
         }
+
+        span.end();
+        Thread.sleep(10000L);
+
 //      
 //      if (context == null) {
 //          spanContext = SpanContext.;
