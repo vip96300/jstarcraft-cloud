3e6090a8003d7fe20785ad2a1b95e171c50f8283
Birdy <Birdy@LAPTOP-22TTFGMB>
2021-07-22 00:35:10 +0800

    重构AmazonStreamManager.

--- jstarcraft-cloud-platform/jstarcraft-cloud-platform-amazon/src/main/java/com/jstarcraft/cloud/platform/amazon/AmazonStreamManager.java
+++ jstarcraft-cloud-platform/jstarcraft-cloud-platform-amazon/src/main/java/com/jstarcraft/cloud/platform/amazon/AmazonStreamManager.java
@@ -1,107 +1,80 @@
 package com.jstarcraft.cloud.platform.amazon;
 
 import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
 
-import com.amazonaws.ClientConfiguration;
-import com.amazonaws.Protocol;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.client.builder.AwsClientBuilder;
-import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.AmazonS3Exception;
-import com.amazonaws.services.s3.model.CreateBucketRequest;
-import com.amazonaws.services.s3.model.DeleteObjectRequest;
-import com.amazonaws.services.s3.model.GetObjectRequest;
-import com.amazonaws.services.s3.model.ListObjectsRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
 import com.jstarcraft.cloud.platform.CloudStreamManager;
 
 public class AmazonStreamManager extends CloudStreamManager {
 
-    private static final String storage = "amazon";
-
-    // private static final String OSS_URL="http://s3.amazonaws.com";
-    private static final String OSS_URL = "http://localhost:8080";
-
-    private BasicAWSCredentials credentials;
-
-    private ClientConfiguration configuration;
-
-    private String bucketName;
-
     private AmazonS3 s3;
 
-    public AmazonStreamManager(String accessKey, String secretKey, String bucketName) {
+    public AmazonStreamManager(String storage, AmazonS3 s3) {
         super(storage);
-        this.credentials = new BasicAWSCredentials(accessKey, secretKey);
-        this.configuration = new ClientConfiguration();
-        this.bucketName = bucketName;
-        // 凭证验证方式
-        this.configuration.setSignerOverride("S3SignerType");
-        // 访问协议
-        this.configuration.setProtocol(Protocol.HTTP);
-        // 设置要用于请求的端点配置（服务端点和签名区域）
-        AwsClientBuilder.EndpointConfiguration endpointConfiguration = new AwsClientBuilder.EndpointConfiguration(OSS_URL, Regions.CN_NORTH_1.toString());
-        s3 = AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(credentials)).withClientConfiguration(configuration).withEndpointConfiguration(endpointConfiguration).withPathStyleAccessEnabled(true).build();
-        // 创建桶(已存在会忽略)
-        CreateBucketRequest createBucketRequest = new CreateBucketRequest(bucketName);
-        s3.createBucket(createBucketRequest);
-    }
-
-    public AmazonStreamManager(BasicAWSCredentials credentials, ClientConfiguration configuration, String bucketName) {
-        super(storage);
-        this.credentials = credentials;
-        this.configuration = configuration;
-        this.bucketName = bucketName;
+        this.s3 = s3;
     }
 
     @Override
     public void saveResource(String path, InputStream stream) {
         ObjectMetadata metadata = new ObjectMetadata();
-        PutObjectRequest request = new PutObjectRequest(bucketName, path, stream, metadata);
-        s3.putObject(request);
+        s3.putObject(storage, path, stream, metadata);
     }
 
     @Override
     public void waiveResource(String path) {
-        DeleteObjectRequest request = new DeleteObjectRequest(bucketName, path);
-        s3.deleteObject(request);
+        s3.deleteObject(storage, path);
     }
 
     @Override
     public boolean haveResource(String path) {
-        return s3.doesObjectExist(bucketName, path);
+        return s3.doesObjectExist(storage, path);
     }
 
     @Override
     public InputStream retrieveResource(String path) {
-        GetObjectRequest request = new GetObjectRequest(bucketName, path);
         try {
-            S3Object object = s3.getObject(request);
+            S3Object object = s3.getObject(storage, path);
             return object.getObjectContent().getDelegateStream();
         } catch (AmazonS3Exception exception) {
             return null;
         }
     }
 
+    private class AmazonStreamIterator implements Iterator<String> {
+
+        private Iterator<S3ObjectSummary> iterator;
+
+        private AmazonStreamIterator(Iterator<S3ObjectSummary> iterator) {
+            this.iterator = iterator;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return iterator.hasNext();
+        }
+
+        @Override
+        public String next() {
+            S3ObjectSummary object = iterator.next();
+            return object.getKey();
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+
+    }
+
     @Override
     public Iterator<String> iterateResources(String path) {
-        ListObjectsRequest request = new ListObjectsRequest(bucketName, path, null, null, Integer.MAX_VALUE);
-        Iterator<S3ObjectSummary> objectSummaryList = s3.listObjects(request).getObjectSummaries().listIterator();
-        List<String> keys = new ArrayList<>();
-        while (objectSummaryList.hasNext()) {
-            String key = objectSummaryList.next().getKey();
-            keys.add(key);
-        }
-        return keys.iterator();
+        Iterator<S3ObjectSummary> iterator = s3.listObjects(storage, path).getObjectSummaries().listIterator();
+        return new AmazonStreamIterator(iterator);
     }
 
 }
--- jstarcraft-cloud-platform/jstarcraft-cloud-platform-amazon/src/test/java/com/jstarcraft/cloud/platform/amazon/AmazonStreamManagerTestsCase.java
+++ jstarcraft-cloud-platform/jstarcraft-cloud-platform-amazon/src/test/java/com/jstarcraft/cloud/platform/amazon/AmazonStreamManagerTestsCase.java
@@ -1,38 +1,63 @@
 package com.jstarcraft.cloud.platform.amazon;
 
-import com.jstarcraft.core.io.StreamManager;
-import com.jstarcraft.core.utility.StringUtility;
-import io.findify.s3mock.S3Mock;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 import org.apache.commons.io.IOUtils;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
+import com.amazonaws.ClientConfiguration;
+import com.amazonaws.Protocol;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.AnonymousAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.jstarcraft.core.io.StreamManager;
+import com.jstarcraft.core.utility.StringUtility;
 
-/**
- * @author Huang Hong Fei
- * @createAt 2021/7/20
- * @description
- */
+import io.findify.s3mock.S3Mock;
+
 public class AmazonStreamManagerTestsCase {
 
-    private static final String accessKey = "1";
+    private static final String storage = "amazon";
 
-    private static final String secretKey = "1";
+    private S3Mock server;
 
-    private static final String bucketName = "test";
-
-    protected StreamManager getStreamManager() {
-        AmazonStreamManager manager = new AmazonStreamManager(accessKey, secretKey, bucketName);
-        return manager;
-    }
+    private AmazonS3 client;
 
     @Before
-    public void setUp() {
-        S3Mock mockServer = S3Mock.create(8080);
-        mockServer.start();
+    public void before() {
+        server = S3Mock.create(8080);
+        server.start();
+        AmazonS3ClientBuilder builder = AmazonS3ClientBuilder.standard();
+        builder.withPathStyleAccessEnabled(true);
+        ClientConfiguration configuration = new ClientConfiguration();
+        configuration.setSignerOverride("S3SignerType");
+        configuration.setProtocol(Protocol.HTTP);
+        builder.withClientConfiguration(configuration);
+        EndpointConfiguration endpoint = new EndpointConfiguration("http://localhost:8080", Regions.CA_CENTRAL_1.getName());
+        builder.withEndpointConfiguration(endpoint);
+        builder.withCredentials(new AWSStaticCredentialsProvider(new AnonymousAWSCredentials()));
+        client = builder.build();
+        client.createBucket(storage);
+    }
+
+    @After
+    public void after() {
+        client.deleteBucket(storage);
+        client.shutdown();
+        server.stop();
+        server.shutdown();
+    }
+
+    protected StreamManager getStreamManager() {
+        AmazonStreamManager manager = new AmazonStreamManager(storage, client);
+        return manager;
     }
 
     @Test
