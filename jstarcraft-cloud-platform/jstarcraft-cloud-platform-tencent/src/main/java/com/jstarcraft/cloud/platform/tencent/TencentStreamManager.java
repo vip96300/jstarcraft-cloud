8c5634cd7262a51963b14e4b26b693593d5843e5
huang hong fei <vip96300@163.com>
2021-07-22 15:05:13 +0800

    tencent oss

---
+++ jstarcraft-cloud-platform/jstarcraft-cloud-platform-tencent/src/main/java/com/jstarcraft/cloud/platform/tencent/TencentStreamManager.java
@@ -0,0 +1,83 @@
+package com.jstarcraft.cloud.platform.tencent;
+
+import com.jstarcraft.cloud.platform.CloudStreamManager;
+import com.qcloud.cos.COSClient;
+import com.qcloud.cos.model.*;
+
+import java.io.InputStream;
+import java.util.Iterator;
+
+/**
+ * @author Huang Hong Fei
+ * @createAt 2021/7/22
+ * @description
+ */
+public class TencentStreamManager extends CloudStreamManager {
+
+    private COSClient client;
+
+    protected TencentStreamManager(String storage) {
+        super(storage);
+    }
+
+    public TencentStreamManager(String storage, COSClient client){
+        this(storage);
+        this.client=client;
+    }
+
+    @Override
+    public void saveResource(String s, InputStream inputStream) {
+        PutObjectRequest request=new PutObjectRequest(storage,s,inputStream,new ObjectMetadata());
+        client.putObject(request);
+    }
+
+    @Override
+    public void waiveResource(String s) {
+        DeleteObjectRequest request=new DeleteObjectRequest(storage,s);
+        client.deleteObject(request);
+    }
+
+    @Override
+    public boolean haveResource(String s) {
+        return client.doesObjectExist(storage,s);
+    }
+
+    @Override
+    public InputStream retrieveResource(String s) {
+        GetObjectRequest request=new GetObjectRequest(storage,s);
+        COSObject object=client.getObject(request);
+        return object.getObjectContent();
+    }
+
+    private class TencentStreamIterator implements Iterator<String> {
+
+        private Iterator<COSObjectSummary> iterator;
+
+        private TencentStreamIterator(Iterator<COSObjectSummary> iterator) {
+            this.iterator = iterator;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return iterator.hasNext();
+        }
+
+        @Override
+        public String next() {
+            COSObjectSummary object = iterator.next();
+            return object.getKey();
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+
+    }
+
+    @Override
+    public Iterator<String> iterateResources(String s) {
+        ObjectListing list=client.listObjects(storage,s);
+        return new TencentStreamIterator(list.getObjectSummaries().iterator());
+    }
+}
---
+++ jstarcraft-cloud-platform/jstarcraft-cloud-platform-tencent/src/test/java/com/jstarcraft/cloud/platform/tencent/TencentStreamManagerTestCase.java
@@ -0,0 +1,86 @@
+package com.jstarcraft.cloud.platform.tencent;
+
+import com.jstarcraft.core.io.StreamManager;
+import com.jstarcraft.core.utility.StringUtility;
+import com.qcloud.cos.COSClient;
+import com.qcloud.cos.ClientConfig;
+import com.qcloud.cos.auth.AnonymousCOSCredentials;
+import com.qcloud.cos.auth.BasicCOSCredentials;
+import com.qcloud.cos.auth.COSCredentials;
+import com.qcloud.cos.auth.COSSigner;
+import com.qcloud.cos.model.CreateBucketRequest;
+import com.qcloud.cos.model.DeleteBucketRequest;
+import com.qcloud.cos.region.Region;
+import org.apache.commons.io.IOUtils;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+/**
+ * @author Huang Hong Fei
+ * @createAt 2021/7/22
+ * @description
+ */
+public class TencentStreamManagerTestCase {
+
+    private static final String storage = "tencent";
+
+    private static final String ACCESS_KEY="AKIDf9RSDhuqVt0qPhiSw83h4AzWmlTt2uVq";
+
+    private static final String SECRET_KEY="5vTkkTw82f15sNcaHhqiJdgTzDAsJSKv";
+
+    private COSClient client;
+
+    @Before
+    public void before() {
+        COSCredentials credentials=new BasicCOSCredentials(ACCESS_KEY,SECRET_KEY);
+        //COSCredentials credentials=new AnonymousCOSCredentials();
+        ClientConfig config=new ClientConfig();
+        config.setCosSigner(new COSSigner());
+        config.setRegion(new Region("ap-beijing"));
+        client=new COSClient(credentials,config);
+        //创建bucket
+        CreateBucketRequest createBucketRequest=new CreateBucketRequest(storage);
+        client.createBucket(createBucketRequest);
+    }
+
+    @After
+    public void after() {
+        DeleteBucketRequest deleteBucketRequest=new DeleteBucketRequest(storage);
+        client.deleteBucket(deleteBucketRequest);
+    }
+
+    protected StreamManager getStreamManager() {
+        TencentStreamManager manager = new TencentStreamManager(storage,client);
+        return manager;
+    }
+
+    @Test
+    public void testHaveResource() throws Exception {
+        StreamManager manager = getStreamManager();
+        String path = "left/middle/right.txt";
+
+        {
+            Assert.assertFalse(manager.haveResource(path));
+            InputStream stream = manager.retrieveResource(path);
+            Assert.assertNull(stream);
+        }
+
+        try (InputStream stream = new ByteArrayInputStream(path.getBytes(StringUtility.CHARSET))) {
+            manager.saveResource(path, stream);
+            Assert.assertTrue(manager.haveResource(path));
+        }
+
+        try (InputStream stream = manager.retrieveResource(path)) {
+            String content = IOUtils.readLines(stream, StringUtility.CHARSET).get(0);
+            Assert.assertEquals(path, content);
+        }
+
+        manager.waiveResource(path);
+        Assert.assertFalse(manager.haveResource(path));
+    }
+}
