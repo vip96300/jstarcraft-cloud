c669946038c001daf4174fd0bf0dff80e28b0a41
Birdy <Birdy@LAPTOP-22TTFGMB>
2021-07-25 13:03:45 +0800

    重构TencentStreamManager

--- jstarcraft-cloud-platform/jstarcraft-cloud-platform-tencent/src/test/java/com/jstarcraft/cloud/platform/tencent/TencentStreamManagerTestCase.java
+++ jstarcraft-cloud-platform/jstarcraft-cloud-platform-tencent/src/test/java/com/jstarcraft/cloud/platform/tencent/TencentStreamManagerTestCase.java
@@ -1,10 +1,22 @@
 package com.jstarcraft.cloud.platform.tencent;
 
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Properties;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.google.common.collect.Lists;
 import com.jstarcraft.core.io.StreamManager;
 import com.jstarcraft.core.utility.StringUtility;
 import com.qcloud.cos.COSClient;
 import com.qcloud.cos.ClientConfig;
-import com.qcloud.cos.auth.AnonymousCOSCredentials;
 import com.qcloud.cos.auth.BasicCOSCredentials;
 import com.qcloud.cos.auth.COSCredentials;
 import com.qcloud.cos.auth.COSSigner;
@@ -12,56 +24,59 @@ import com.qcloud.cos.model.CannedAccessControlList;
 import com.qcloud.cos.model.CreateBucketRequest;
 import com.qcloud.cos.model.DeleteBucketRequest;
 import com.qcloud.cos.region.Region;
-import org.apache.commons.io.IOUtils;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
 
 /**
- * @author Huang Hong Fei
- * @createAt 2021/7/22
- * @description
+ * Huang Hong Fei
+ * 
+ * @author Birdy
+ *
  */
 public class TencentStreamManagerTestCase {
 
-    /**
-     * 规范:{bucketName}-{appid}
-     */
-    private static final String storage = "default-1306604551";
+    /** {bucketName}-{appid} */
+    private static String storage;
 
-    private static final String ACCESS_KEY="AKIDf9RSDhuqVt0qPhiSw83h4AzWmlTt2uVq";
+    private static String accessKey;
 
-    private static final String SECRET_KEY="5vTkkTw82f15sNcaHhqiJdgTzDAsJSKv";
+    private static String secretKey;
 
-    private COSClient client;
+    private static String region;
 
-    @Before
-    public void before() {
-        COSCredentials credentials=new BasicCOSCredentials(ACCESS_KEY,SECRET_KEY);
-        //COSCredentials credentials=new AnonymousCOSCredentials();
-        ClientConfig config=new ClientConfig();
-        config.setCosSigner(new COSSigner());
-        config.setRegion(new Region("ap-chongqing"));
-        client=new COSClient(credentials,config);
-        //创建bucket
-        CreateBucketRequest createBucketRequest=new CreateBucketRequest(storage);
-        //共有
-        createBucketRequest.setCannedAcl(CannedAccessControlList.PublicReadWrite);
-        client.createBucket(createBucketRequest);
+    static {
+        try {
+            Properties keyValues = new Properties();
+            keyValues.load(new FileInputStream("cos.properties"));
+            storage = keyValues.getProperty("storage");
+            accessKey = keyValues.getProperty("access_key");
+            secretKey = keyValues.getProperty("secret_key");
+            region = keyValues.getProperty("region");
+        } catch (Exception exception) {
+        }
     }
 
-    @After
-    public void after() {
-        DeleteBucketRequest deleteBucketRequest=new DeleteBucketRequest(storage);
-        client.deleteBucket(deleteBucketRequest);
+    private static COSClient cos;
+
+    @BeforeClass
+    public static void before() throws Exception {
+        COSCredentials credentials = new BasicCOSCredentials(accessKey, secretKey);
+        ClientConfig configuration = new ClientConfig();
+        configuration.setCosSigner(new COSSigner());
+        configuration.setRegion(new Region(region));
+        cos = new COSClient(credentials, configuration);
+
+        CreateBucketRequest createBucketRequest = new CreateBucketRequest(storage);
+        createBucketRequest.setCannedAcl(CannedAccessControlList.PublicReadWrite);
+        cos.createBucket(createBucketRequest);
+    }
+
+    @AfterClass
+    public static void after() {
+        DeleteBucketRequest deleteBucketRequest = new DeleteBucketRequest(storage);
+        cos.deleteBucket(deleteBucketRequest);
     }
 
     protected StreamManager getStreamManager() {
-        TencentStreamManager manager = new TencentStreamManager(storage,client);
+        TencentStreamManager manager = new TencentStreamManager(storage, cos);
         return manager;
     }
 
@@ -89,4 +104,69 @@ public class TencentStreamManagerTestCase {
         manager.waiveResource(path);
         Assert.assertFalse(manager.haveResource(path));
     }
+
+    @Test
+    public void testIterateResources() throws Exception {
+        StreamManager manager = getStreamManager();
+        String[] types = { "protoss", "terran", "zerg" };
+        for (String type : types) {
+            String path = "jstarcraft/" + type + ".txt";
+            // 保存资源
+            try (InputStream stream = new ByteArrayInputStream(type.getBytes(StringUtility.CHARSET))) {
+                manager.saveResource(path, stream);
+                Assert.assertTrue(manager.haveResource(path));
+            }
+        }
+        for (String type : types) {
+            String path = "jstarcraft/" + type + "/" + type + ".txt";
+            // 保存资源
+            try (InputStream stream = new ByteArrayInputStream(type.getBytes(StringUtility.CHARSET))) {
+                manager.saveResource(path, stream);
+                Assert.assertTrue(manager.haveResource(path));
+            }
+        }
+
+        Assert.assertFalse(manager.haveResource("jstarcraft/"));
+        Assert.assertFalse(manager.haveResource("jstarcraft/protoss/"));
+        Assert.assertFalse(manager.haveResource("jstarcraft/terran/"));
+        Assert.assertFalse(manager.haveResource("jstarcraft/zerg/"));
+
+        List<String> resources = Lists.newArrayList(manager.iterateResources("jstarcraft/"));
+        Assert.assertEquals(6, resources.size());
+
+        for (String type : types) {
+            String path = "jstarcraft/" + type + "/" + type + ".txt";
+            // 获取资源
+            try (InputStream stream = manager.retrieveResource(path)) {
+                String content = IOUtils.readLines(stream, StringUtility.CHARSET).get(0);
+                Assert.assertEquals(type, content);
+            }
+        }
+
+        for (String type : types) {
+            String path = "jstarcraft/" + type + "/" + type + ".txt";
+            // 废弃资源
+            manager.waiveResource(path);
+            Assert.assertFalse(manager.haveResource(path));
+        }
+
+        resources = Lists.newArrayList(manager.iterateResources("jstarcraft/"));
+        Assert.assertEquals(3, resources.size());
+
+        // 废弃资源
+        manager.waiveResource("jstarcraft/");
+        resources = Lists.newArrayList(manager.iterateResources("jstarcraft/"));
+        Assert.assertEquals(3, resources.size());
+
+        for (String type : types) {
+            String path = "jstarcraft/" + type + ".txt";
+            // 废弃资源
+            manager.waiveResource(path);
+            Assert.assertFalse(manager.haveResource(path));
+        }
+
+        resources = Lists.newArrayList(manager.iterateResources("jstarcraft/"));
+        Assert.assertEquals(0, resources.size());
+    }
+
 }
